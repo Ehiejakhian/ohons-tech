497ab6711bd2277c980dfa429e8ce798a0545ca1
Ehi Ejakhian <ehiejakhian@gmail.com>
2026-04-17 17:06:28 +0100

    new stuff

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,5 +1,6 @@
 
 let root = document.querySelector(':root');
+let themeBtn = document.querySelector('#theme-btn');
 
 let rootStyle = root.style
 document.querySelector('#theme-btn').addEventListener('click', () => {
@@ -18,7 +19,6 @@ function checkCurrentTheme() {
     console.log(theme);
     (theme == 'light')? changeTheme('dark'): changeTheme('light');
     console.log(JSON.parse(localStorage.getItem("ohons-tech-theme")));
-  
   }
 }
 
@@ -35,17 +35,22 @@ function changeTheme(theme) {
     rootStyle.setProperty('--white', '#fff');
     rootStyle.setProperty('--light-white', '#ffffffbb');
     
+    themeBtn?themeBtn.classList.remove('dark'): null;
+    themeBtn?themeBtn.classList.add('light'): null;
   } else {
     rootStyle.setProperty('--text', '#fffcee')
     rootStyle.setProperty('--bg', '#121212')
     
     rootStyle.setProperty('--medium', '#c9bebe');
-    rootStyle.setProperty('--dark', '#eae2fc');
+    rootStyle.setProperty('--dark', '#fffadc');
     
     //For Text on white bg
     rootStyle.setProperty('--text-on-bg', '#080808');
     rootStyle.setProperty('--white', '#acacac');
     rootStyle.setProperty('--light-white', '#121212bb');
+    
+    themeBtn ? themeBtn.classList.remove('light') : null;  
+    themeBtn?themeBtn.classList.add('dark'): null;
   }
   localStorage.setItem("ohons-tech-theme", JSON.stringify(theme));
 }
